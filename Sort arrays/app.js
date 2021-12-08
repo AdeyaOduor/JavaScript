90168255af90d8432b32288843fc9c1cafee3050
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-12-09 01:14:08 +0300

    Update app.js

--- Sort arrays/app.js
+++ Sort arrays/app.js
@@ -1,3 +1,11 @@
+// A JavaScript program to get a sorted array of objects ordered by properties and orders.
+
+// Uses Array.prototype.sort(), Array.prototype.reduce() on the props array with a default value of 0.
+// Use array destructuring to swap the properties position depending on the order supplied.
+// If no orders array is supplied, sort by 'asc' by default.
+
+
+
 const orderBy = (arr, props, orders) =>
   [...arr].sort((a, b) =>
     props.reduce((acc, prop, i) => {
