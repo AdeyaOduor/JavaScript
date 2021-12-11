82e7462197a81756e269404ab8d88d805beda5e8
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-12-12 00:21:36 +0300

    Update random.js

--- randInt/random.js
+++ randInt/random.js
@@ -1,3 +1,6 @@
+//JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. 
+//If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
+
 // Get a random integer from 1 to 10 inclusive
 const num = Math.ceil(Math.random() * 10);
 console.log(num);
