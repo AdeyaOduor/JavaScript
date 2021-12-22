6300c8ca0594e71b395923bd60f7e2ab4da38c15
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-12-22 21:15:05 +0300

    Create emailValidator.js

---
+++ URLify/emailValidator.js
@@ -0,0 +1,16 @@
+ // pattern that matches e-mail addresses.
+
+function valid_email(str)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(mailformat.test(str))
+{  
+console.log("Valid email address!");  
+}  
+else  
+{  
+console.log("You have entered an invalid email address!");  
+}
+}
+
+valid_email('me-info@example.com');
