a15d95745708863c81aac57e99e2759f53184cd0
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-12-26 22:43:03 +0300

    Create multiplication_table01.js

---
+++ maths/multiplication_table01.js
@@ -0,0 +1,10 @@
+
+const multiTable = (number) => {
+  let table = '';
+  
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+
+  return table;
+}
