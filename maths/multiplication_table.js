f3f16f3cba5b51b9b89b422065e8051fd278f5ae
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-06-24 00:09:20 +0300

    Update multiplication_table.js

--- maths/multiplication_table.js
+++ maths/multiplication_table.js
@@ -12,3 +12,15 @@ for(let i = 1; i <= range; i++) {
     const result = i * number;
     console.log(`${number} * ${i} = ${result}`);
 }
+
+
+// or
+const multiTable = (number) => {
+  let table = '';
+  
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+
+  return table;
+}
