2bfd3b9639da2abc3dc476dbfbafa3d50dc68ad6
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-07-02 21:59:46 +0300

    Update oddValues.js

--- maths/oddValues.js
+++ maths/oddValues.js
@@ -1,11 +1,1 @@
-function odds(values){
- 
-    return values.filter(values => values % 2 !== 0 );
-    }
-    odds([1, 2, 3, 4, 5, 6, 7, 8, 9]); 
 
-///////////
-
-function findOdd(arr) {
-    return arr.find((item, index) => arr.filter(el => el == item).length % 2)
-  }
