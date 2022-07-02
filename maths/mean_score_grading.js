3210b773c21c39e0125eaf8d0cc7380ca66ba87e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-07-03 00:49:02 +0300

    Update mean_score_grading.js

--- maths/mean_score_grading.js
+++ maths/mean_score_grading.js
@@ -25,3 +25,21 @@ console.log("Average grade: " + (Avgmarks)/students.length);
         } else if (avg < 100) {
                 console.log("Grade : A")
         };
+
+// method2
+function grades(students) {
+    students.forEach(function(student) {
+      let avg = (student.grades.reduce((acc, cur) => acc + cur)) / student.grades.length;
+      let grade = "";
+      if (avg < 50) { grade = "F"; }
+      else if (avg < 55) { grade = "D"; }
+      else if (avg < 65) { grade = "C"; }
+      else if (avg < 75) { grade = "B"; }
+      else { grade = "A"; }
+      console.log(`student: ${student.name}, average score: ${Math.round(avg)}, grade: ${grade}`);
+    })
+  }
+  let students = [
+  {name: "Joey", grades: [65,78,82]}, {name: "Johnny", grades: [84,90, 92]}, {name: "DeeDee", grades: [62, 58, 71]}, {name: "Tommy", grades: [92,94,96]}
+  ]
+  grades(students);
