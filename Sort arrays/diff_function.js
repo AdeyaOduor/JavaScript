70e9746ded9723f1606165342335a1b49923f25e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-03 01:31:18 +0300

    Update diff_function.js

--- Sort arrays/diff_function.js
+++ Sort arrays/diff_function.js
@@ -1,5 +1,4 @@
 // implement a difference function which subtracts one list from another and returns the result
-
 function array_diff(a, b) {
   return a.filter(function(x) { return b.indexOf(x) == -1; });
 }
