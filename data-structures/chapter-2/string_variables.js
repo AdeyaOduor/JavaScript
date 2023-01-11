5975a153e6e7c9306dce7451ae335ea72b425940
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-11 09:11:41 +0300

    Update string_variables.js

--- data-structures/chapter-2/string_variables.js
+++ data-structures/chapter-2/string_variables.js
@@ -1,5 +1,14 @@
-var myFirstName = "David";
-var myLastName = "Oduor";
+/* When JavaScript variables are declared, they have an initial value of undefined. A mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.*/
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
 
 // initializing variables
 var a = 5;
@@ -10,4 +19,3 @@ a = a + 1;
 b = b + 5;
 c = c + " String!";
 
-/* When JavaScript variables are declared, they have an initial value of undefined. A mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.*/
