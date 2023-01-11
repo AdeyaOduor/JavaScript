5072e0bea92229146b2f7e428ccf12524b0f5766
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-11 09:02:22 +0300

    Update string_variables.js

--- data-structures/chapter-2/string_variables.js
+++ data-structures/chapter-2/string_variables.js
@@ -1,2 +1,13 @@
 var myFirstName = "David";
 var myLastName = "Oduor";
+
+// initializing variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/* When JavaScript variables are declared, they have an initial value of undefined. A mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.*/
