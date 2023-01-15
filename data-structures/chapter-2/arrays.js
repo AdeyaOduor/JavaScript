9e7db6898ce645ee9e0e1f356c7d0d0f5eb1aeb5
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-16 00:07:02 +0300

    Update arrays.js

--- data-structures/chapter-2/arrays.js
+++ data-structures/chapter-2/arrays.js
@@ -1,1 +1,5 @@
-
+// creating ana aray with a string and an integer
+const myArray = ["Quincy", 1];
+const myArray = [["Bulls", 23], ["White Sox", 45]]; // nested array
+const myArray = [50, 60, 70];
+const myData = myArray[0]; // 50
