28d1da1173ecaee01519766412c26d48b5ce4d4a
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-17 21:35:12 +0300

    Update variables.js

--- data-structures/chapter-2/variables.js
+++ data-structures/chapter-2/variables.js
@@ -42,11 +42,11 @@ a /= 12;
 b /= 4;
 c /= 11; // output is a=4, b=27 and c=3 repectively
 
+/*Compound Assignment With Augmented Addition, multiplication, division and subtraction respectively*/
 let a = 3;
 let b = 17;
 let c = 12;
 
-/*Compound Assignment With Augmented Addition, multiplication, division and subtraction respectively*/
 a += 12;
 b += 9;
 c += 7;
