45993eb03b582d8a471844184d3900f902b0a438
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-20 00:03:39 +0300

    Update functions.js

--- data-structures/chapter-2/functions.js
+++ data-structures/chapter-2/functions.js
@@ -67,3 +67,27 @@ function addFive() {
 
 addThree();
 addFive(); // output is 8
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+processed = processArg(7); // Equal to 2
+
+/*a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Add the number to the end of the array, then remove the first element of the array.
+The nextInLine function should then return the element that was removed.*/
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift(); 
+}
+ 
+var testArr = [1,2,3,4,5];
+ 
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
