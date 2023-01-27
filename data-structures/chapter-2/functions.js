1bb41866f2da8a38eae4f6a69e2e602df42fa7c3
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-28 01:42:42 +0300

    Update functions.js

--- data-structures/chapter-2/functions.js
+++ data-structures/chapter-2/functions.js
@@ -16,43 +16,6 @@ function timesFive(num) {
   return num * 5;
 }
 
-// global variables
-let myGlobal = 10;
-
-function fun1() {
-  oopsGlobal = 5;
-}
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
-// local variables
-function myLocalScope() {
-  var myVar = 5;
-  console.log(myVar);
-}
-myLocalScope();
-
-console.log(myVar);
-
-/* It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.*/
-var outerWear = "T-Shirt";
-
-function myOutfit() {
-  var outerWear = "sweater";
-
-  return outerWear;
-}
-
-console.log(myOutfit());
-console.log(outerWear);
 
 /*A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.*/
 var sum = 0;
