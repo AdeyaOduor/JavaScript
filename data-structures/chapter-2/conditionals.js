f80c3f6176a0321a94984dcdd5c24908aa8a2a48
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-01-29 22:21:47 +0300

    Update conditionals.js

--- data-structures/chapter-2/conditionals.js
+++ data-structures/chapter-2/conditionals.js
@@ -1,1 +1,42 @@
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
+// Change this value to test
+testStrict(10);
+
+function testNotEqual(val) {
+  if (val!= 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+function testStrictNotEqual(val) {
+  if (val!== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+  
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+console.log(testGreaterThan(10));
