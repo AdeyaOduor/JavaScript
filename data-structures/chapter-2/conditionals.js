09b38d414c1735e7c699e4eee529b481f183a431
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-02-02 00:03:51 +0300

    Update conditionals.js

--- data-structures/chapter-2/conditionals.js
+++ data-structures/chapter-2/conditionals.js
@@ -110,3 +110,31 @@ function testLogicalOr(val) {
 
 // Change this value to test
 testLogicalOr(15);
+
+
+function testElse(val) {
+  let result = "";
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }else {
+    result = "5 or Smaller";
+  }
+
+  return result;
+}
+
+testElse(4);
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(7);
