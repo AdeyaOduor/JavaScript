1d597c0ce7662f56ab93035112cb21857343207e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-02-04 01:05:58 +0300

    Update conditionals.js

--- data-structures/chapter-2/conditionals.js
+++ data-structures/chapter-2/conditionals.js
@@ -214,3 +214,76 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(3);
+
+function switchOfStuff(val) {
+  let answer = "";
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Multiple Identical Options in Switch Statements
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+  return answer;
+
+}
+sequentialSizes(1);
+
+function chainToSwitch(val) {
+  var answer = "";
+  // changing else if to switch
+  
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  
+
+  return answer;  
+}
+
+// Change this value to test
+chainToSwitch(7);
