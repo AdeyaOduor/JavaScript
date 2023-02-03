50b00daef97162dabb3df4f979f3389230617464
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-02-03 09:38:23 +0300

    Update conditionals.js

--- data-structures/chapter-2/conditionals.js
+++ data-structures/chapter-2/conditionals.js
@@ -163,3 +163,54 @@ function testSize(num) {
   }
 
 }
+
+// multiple else if conditionals
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+
+}
+
+// Change these values to test
+golfScore(5, 4);
+
+function caseInSwitch(val) {
+  let answer = "";
+
+   switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  return answer;  
+
+
+  return answer;
+}
+
+caseInSwitch(3);
