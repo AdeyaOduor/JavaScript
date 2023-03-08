009bfacec1af551197544b3fcc7a8192ae85b840
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-08 19:54:21 +0300

    Update regex.js

--- codewarPractice/regex.js
+++ codewarPractice/regex.js
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to check whether a given value is an valid email or not</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
 function validate(password) {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password);
 }
