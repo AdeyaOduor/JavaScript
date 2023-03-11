aec5194b91cbcac8ed19987a77409bbeba2c6378
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-11 23:40:43 +0300

    Update csv_to_array.js

--- csv_to_array.js
+++ csv_to_array.js
@@ -1,1 +1,11 @@
+//#Source https://bit.ly/2neWfJ2
+const csv_to_array = (data, delimiter = ',', omitFirstRow = false) =>
+  data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map(v => v.split(delimiter));
+
+console.log(csv_to_array('a,b\nc,d')); 
+console.log(csv_to_array('a;b\nc;d', ';')); 
+console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
 
