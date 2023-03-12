04667dfad9a93f441f1576b8e915829fb5539093
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-12 22:59:40 +0300

    Update pull_at_Index.js

--- data-structures/chapter-2/pull_at_Index.js
+++ data-structures/chapter-2/pull_at_Index.js
@@ -1,1 +1,15 @@
+//#Source https://bit.ly/2neWfJ2 
+const pull_at_Index = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach(v => arr.push(v));
+  return removed;
+};
+let arra1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(pull_at_Index(arra1, [1, 3]));
+let arra2 =  [1, 2, 3, 4, 5, 6, 7];
+console.log(pull_at_Index(arra2, [4]));
 
