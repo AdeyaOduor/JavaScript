3add735f1e9e6639c5377358b28b70f382b61a4e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-14 19:10:22 +0300

    Delete min_max.js

--- data-structures/chapter-2/min_max.js
+++
@@ -1,9 +0,0 @@
-//program to return the minimum-maximum value of an array, after applying the provided function to set comparing rule. 
-const reduce_Which = (arr, comparator = (a, b) => a - b) =>
-  arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
-console.log(reduce_Which([1, 3, 2])); 
-console.log(reduce_Which([10, 30, 20], (a, b) => b - a));  
-console.log(reduce_Which(
-  [{ name: 'Kevin', age: 16 }, { name: 'John', age: 20 }, { name: 'Ani', age: 19 }],
-  (a, b) => a.age - b.age)); 
-
