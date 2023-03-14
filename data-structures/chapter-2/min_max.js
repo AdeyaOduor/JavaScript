90bd5c1b90d35798449eba1cfe1398ad042b41c7
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-14 19:05:40 +0300

    Update min_max.js

--- data-structures/chapter-2/min_max.js
+++ data-structures/chapter-2/min_max.js
@@ -1,1 +1,9 @@
+//#Source https://bit.ly/2neWfJ2 
+const reduce_Which = (arr, comparator = (a, b) => a - b) =>
+  arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
+console.log(reduce_Which([1, 3, 2])); 
+console.log(reduce_Which([10, 30, 20], (a, b) => b - a));  
+console.log(reduce_Which(
+  [{ name: 'Kevin', age: 16 }, { name: 'John', age: 20 }, { name: 'Ani', age: 19 }],
+  (a, b) => a.age - b.age)); 
 
