5a3adae141eb98f75e529cdfb4e36432edbba20b
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-15 21:53:34 +0300

    Create csv_to_array.js

---
+++ maths/csv_to_array.js
@@ -0,0 +1,10 @@
+//program to convert a comma-separated values (CSV) string to a 2D array
+const csv_to_array = (data, delimiter = ',', omitFirstRow = false) =>
+  data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map(v => v.split(delimiter));
+
+console.log(csv_to_array('a,b\nc,d')); 
+console.log(csv_to_array('a;b\nc;d', ';')); 
+console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
