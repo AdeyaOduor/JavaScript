255aec37331123cd606f8fd4418e1981650acac0
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-22 14:08:24 +0300

    Update numbers_ranges.js

--- maths/numbers_ranges.js
+++ maths/numbers_ranges.js
@@ -1,1 +1,17 @@
 
+function numbers_ranges(x, y) {
+  if ((x >= 40 && x <= 60 && y >= 40 && y <= 60) 
+      || 
+      (x >= 70 && x <= 100 && y >= 70 && y <= 100))
+     {
+    return true;
+     } 
+    else 
+     {
+    return false;
+  }
+}
+
+console.log(numbers_ranges(44, 56));
+console.log(numbers_ranges(70, 95));
+console.log(numbers_ranges(50, 89));
