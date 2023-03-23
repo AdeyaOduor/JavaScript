e5ffa8ce716b149d44fcdaa2d21d05c56af5700f
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-24 00:35:58 +0300

    Update filter_Non_Unique.js

--- Sort arrays/filter_Non_Unique.js
+++ Sort arrays/filter_Non_Unique.js
@@ -1,1 +1,6 @@
+//#Source https://bit.ly/2neWfJ2 
+const filter_Non_Unique = arr => arr.filter(i => arr.indexOf(i) === arr.lastIndexOf(i));
+
+console.log(filter_Non_Unique([1, 2, 2, 3, 4, 4, 5]));  
+console.log(filter_Non_Unique([1, 2, 3, 4]));  
 
