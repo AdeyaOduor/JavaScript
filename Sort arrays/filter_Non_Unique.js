30f5fee3067adfc6fc29769b8e4acc463351f98e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-24 00:38:16 +0300

    Update filter_Non_Unique.js

--- Sort arrays/filter_Non_Unique.js
+++ Sort arrays/filter_Non_Unique.js
@@ -5,3 +5,8 @@ const filter_Non_Unique = arr => arr.filter(i => arr.indexOf(i) === arr.lastInde
 console.log(filter_Non_Unique([1, 2, 2, 3, 4, 4, 5]));  
 console.log(filter_Non_Unique([1, 2, 3, 4]));  
 
+/* Use new Set() and the spread operator (...) to create an array of the unique values in arr.
+Use Array.prototype.filter() to create an array containing only the unique values.
+
+[1,3,5]
+[1,2,3,4] */
