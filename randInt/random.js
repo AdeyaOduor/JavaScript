ae580a927d3f8d6d6f7d913bb3408469b9c84fb1
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-03-28 22:50:02 +0300

    Update random.js

--- randInt/random.js
+++ randInt/random.js
@@ -10,3 +10,17 @@ console.log(num);
   else
    console.log('Not matched, the number was '+gnum);
    
+rand = function(min, max) {
+  if (min==null && max==null)
+    return 0;    
+  
+  if (max == null) {
+      max = min;
+      min = 0;
+    }
+    return min + Math.floor(Math.random() * (max - min + 1));
+  };
+ console.log(rand(20,1));
+ console.log(rand(1,10));
+ console.log(rand(6));
+ console.log(rand());
