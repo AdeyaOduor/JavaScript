52ae40351ea2aac49049a182bc6cd80a25b0b88a
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-03 16:48:06 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -7,3 +7,12 @@ function removeFirstTwo(list) {
 
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source)
+
+// use-destructuring-assignment-to-assign-variables-from-arrays.
+
+let a = 8, b = 6;
+ [a,b] = [b,a];
+
+console.log(a); // should be 6
+console.log(b); // should be 8
+    
