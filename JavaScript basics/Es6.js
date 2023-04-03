03787d685ba0592fc1c2cb026d35c0f45d60f744
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-03 17:42:53 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -16,3 +16,18 @@ let a = 8, b = 6;
 console.log(a); // should be 6
 console.log(b); // should be 8
     
+// create-strings-using-template-literals.
+/*Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and 
+have a class attribute with the value text-warning. The makeList function should return the array of list item strings. 
+*/
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  return arr.map(val => `<li class="text-warning">${val}</li>`);
+}
+
+const failuresList = makeList(result.failure);
