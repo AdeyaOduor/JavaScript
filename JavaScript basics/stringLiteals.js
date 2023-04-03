d320d14e65c3719d53c734bc990ac6bbfb1f8270
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-03 16:41:12 +0300

    Update and rename create-strings-using-template-literals.txt to stringLiteals.js

--- JavaScript basics/stringLiteals.js
+++ JavaScript basics/stringLiteals.js
@@ -1,5 +1,6 @@
 /*Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and 
-have a class attribute with the value text-warning. The makeList function should return the array of list item strings. */
+have a class attribute with the value text-warning. The makeList function should return the array of list item strings. 
+create-strings-using-template-literals.*/
 
 const result = {
   success: ["max-length", "no-amd", "prefer-arrow-functions"],
