9074415bdb56dba6f897db3e52ba4abc00777d3c
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-08 13:37:20 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -81,10 +81,10 @@ stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
 
 /* The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. 
-Use the import * as syntax to import everything from the file into an object called stringFunctions.
+Use the import * as syntax to import everything from the file into an object called stringFunctions */
 
 import subtract from "./math_functions.js";  
 
 subtract(7,4);
 /* In the following code, import the default export from the math_functions.js file, 
-found in the same directory as this file. Give the import the name subtract.
+found in the same directory as this file. Give the import the name subtract */
