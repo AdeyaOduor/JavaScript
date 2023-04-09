db875770088c37ad982a7cedb398d42f54b734cb
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-09 11:55:26 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -65,30 +65,29 @@ export const lowercaseString = (string) => {
   return string.toLowerCase()
 }
 
-// There are two string-related functions in the editor. Export both of them using the method of your choice.
+/* import statement allowing the current file to use the uppercaseString and lowercaseString functions you exported.
+These functions are in a file called string_functions.js, which is in the same directory as the current file.*/
 
 import { uppercaseString, lowercaseString  } from './string_functions.js';  
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
 
-/* import statement allowing the current file to use the uppercaseString and lowercaseString functions you exported.
-These functions are in a file called string_functions.js, which is in the same directory as the current file.*/
+/* The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. 
+Use the import * as syntax to import everything from the file into an object called stringFunctions */
 
 import * as stringFunctions from "./string_functions.js";
 
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
 
-/* The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. 
-Use the import * as syntax to import everything from the file into an object called stringFunctions */
-
+/* In the following code, import the default export from the math_functions.js file, 
+found in the same directory as this file. Give the import the name subtract */
 import subtract from "./math_functions.js";  
 
 subtract(7,4);
-/* In the following code, import the default export from the math_functions.js file, 
-found in the same directory as this file. Give the import the name subtract */
 
+<!-- A script that uses this module type can now use the import and export features -->
 <html>
   <body>
     
@@ -96,4 +95,4 @@ found in the same directory as this file. Give the import the name subtract */
   </body>
 </html>
 
-<!-- A script that uses this module type can now use the import and export features -->
+
