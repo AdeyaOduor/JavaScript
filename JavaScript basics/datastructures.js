f78510e0a789df6c83ab49c8d225d191ed9847b8
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-04-14 23:02:30 +0300

    Update datastructures.js

--- JavaScript basics/datastructures.js
+++ JavaScript basics/datastructures.js
@@ -111,3 +111,30 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(["IV", 5, "six"]));
+
+
+// Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+  return arr.indexOf(elem) != -1;
+}
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+
+// Iterate Through All an Array's Items Using For Loops
+
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // change code below this line
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      //Checks every parameter for the element and if is NOT there continues the code
+      newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
+    }
+  }
+
+  // change code above this line
+  return newArr;
+}
+// change code here to test different cases:
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
