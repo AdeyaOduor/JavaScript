4009709170dbc5db1fe62e40e52d93f3506d6310
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-08 19:56:00 +0300

    Update reverseString.js

--- stings/reverseString.js
+++ stings/reverseString.js
@@ -13,4 +13,4 @@ Using the split() function will turn our string into an array of characters, kee
 Next we chain the reverse() function, which takes our array of characters and reverses them.
 
 Finally, we chain join('') to put our characters back together into a string. Notice once again that we left no spaces in the argument for join, 
-this makes sure that the array of characters is joined back together by each character.
+this makes sure that the array of characters is joined back together by each character. */
