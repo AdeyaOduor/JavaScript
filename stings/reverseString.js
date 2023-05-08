dae1081d9d529f201bf9ea126e66080ec0d5d2b1
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-08 19:55:21 +0300

    Update reverseString.js

--- stings/reverseString.js
+++ stings/reverseString.js
@@ -4,3 +4,13 @@ function reverseString(str) {
     .reverse()
     .join("");
 }
+
+/* Our goal is to take the input, str, and return it in reverse. Our first step is to split the string by characters using split(''). 
+Notice that we don’t leave anything in between the single quotes, this tells the function to split the string by each character.
+
+Using the split() function will turn our string into an array of characters, keep that in mind as we move forward.
+
+Next we chain the reverse() function, which takes our array of characters and reverses them.
+
+Finally, we chain join('') to put our characters back together into a string. Notice once again that we left no spaces in the argument for join, 
+this makes sure that the array of characters is joined back together by each character.
