74686cea339b7de2624c818b17e72a3ed523a4cc
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-09 22:39:28 +0300

    Update factorials.js

--- maths/factorials.js
+++ maths/factorials.js
@@ -5,3 +5,18 @@ function factorial(x, runningTotal = 1 ) {
 }
 
 console.log(factorial(10))
+
+// recursive
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+factorialize(5);
+
+/* Notice at the first line we have the terminal condition, i.e a condition to check the end of the recursion. 
+If num == 0, then we return 1, i.e. effectively ending the recursion and informing the stack to propagate this 
+value to the upper levels. If we do not have this condition, the recursion would go on until the stack space gets 
+consumed, thereby resulting in a Stack Overflow */
