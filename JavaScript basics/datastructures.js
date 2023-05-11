eb9317709c48d26c7a87113e7fbd047be946521f
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-11 20:24:53 +0300

    Update datastructures.js

--- JavaScript basics/datastructures.js
+++ JavaScript basics/datastructures.js
@@ -282,6 +282,10 @@ function addFriend(userObj, friend) {
 
 console.log(addFriend(user, "Pete"));
 
+/*You are given two arrays and an index.
+Copy each element of the first array into the second array, in order.
+Begin inserting elements at index n of the second array.
+Return the resulting array. The input arrays should remain the same after the function runs.*/
 function frankenSplice(arr1, arr2, n) {
   return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
 }
