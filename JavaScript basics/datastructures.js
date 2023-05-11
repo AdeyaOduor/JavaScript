979881d71db0c200fd68031f6df5d0fcc61225d9
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-11 20:22:24 +0300

    Update datastructures.js

--- JavaScript basics/datastructures.js
+++ JavaScript basics/datastructures.js
@@ -281,3 +281,7 @@ function addFriend(userObj, friend) {
 }
 
 console.log(addFriend(user, "Pete"));
+
+function frankenSplice(arr1, arr2, n) {
+  return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+}
