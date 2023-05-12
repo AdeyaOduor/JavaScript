11735b465d4f4d3ae07a11861e6b2dc1e4cdc954
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-12 15:22:06 +0300

    Update min_max.js

--- maths/min_max.js
+++ maths/min_max.js
@@ -30,3 +30,8 @@ function largestOfFour(arr, finalArr = []) {
 
 /*Count the number of entries that are smaller than the new value num
 The new value would be inserted after these values*/
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num > val).length;
+}
+
+getIndexToIns([40, 60], 50);
