11739b620664a0efc58ba3eba322234a6adcda50
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-13 19:42:52 +0300

    Update fibonacci.js

--- maths/fibonacci.js
+++ maths/fibonacci.js
@@ -1,1 +1,4 @@
-
+// fibonaci using reduce method
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`); //output= Sum of array values is: 6
