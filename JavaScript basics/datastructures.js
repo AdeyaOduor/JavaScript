1e025a195590eff3dbc123b11c2d6212228d7f33
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-13 19:35:29 +0300

    Update datastructures.js

--- JavaScript basics/datastructures.js
+++ JavaScript basics/datastructures.js
@@ -99,6 +99,15 @@ console.log(
 
 /*Write a function that splits an array (first argument) into groups the length of size (second argument) 
 and returns them as a two-dimensional array.*/
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 // fibonaci using reduce method
 let myArray = [1, 2, 3];
