7a53aa92ab2f05d7fc199f622ff8f7380f63d19d
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-05-14 13:43:47 +0300

    Update titleCase.js

--- stings/titleCase.js
+++ stings/titleCase.js
@@ -1,2 +1,7 @@
 /* Return the provided string with the first letter of each word capitalized. 
 Make sure the rest of the word is in lower case.*/
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/(^|\s)\S/g, L => L.toUpperCase());
+}
