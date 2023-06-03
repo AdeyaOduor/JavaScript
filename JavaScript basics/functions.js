97cf4972864fdbf289b5502d54c96e73783e0a9a
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-03 23:09:48 +0300

    Update functions.js

--- JavaScript basics/functions.js
+++ JavaScript basics/functions.js
@@ -151,10 +151,7 @@ console.log(finalTabs.tabs);
 var fixedValue = 4;
 
 function incrementer() {
-  // Add your code below this line
   return fixedValue + 1;
-
-  // Add your code above this line
 }
 
 var newValue = incrementer(); // Should equal 5
