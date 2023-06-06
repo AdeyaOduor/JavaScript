97451956016655aa341a46b325e9c1895fb3d5bd
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-06 22:45:59 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -42,7 +42,6 @@ console.log(half(stats)); // should be 28.015
 /*Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and 
 have a class attribute with the value text-warning. The makeList function should return the array of list item strings. 
 */
-
 const result = {
   success: ["max-length", "no-amd", "prefer-arrow-functions"],
   failure: ["no-var", "var-on-top", "linebreak"],
