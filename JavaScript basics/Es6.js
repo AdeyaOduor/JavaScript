865b65ed230ce7920efaad6c2e0f88f6e4b048fd
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-06 22:46:50 +0300

    Update Es6.js

--- JavaScript basics/Es6.js
+++ JavaScript basics/Es6.js
@@ -63,7 +63,6 @@ export const lowercaseString = (string) => {
 
 /*Use object property shorthand with object literals to create and return an object 
 with name, age and gender properties. */
-
 const createPerson = (name, age, gender) => {
   return {name,age,gender};
 };
