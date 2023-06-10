b38cd40b1f3b6ec35b89cd03a5e41447a937b718
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-10 20:36:26 +0300

    Update filter_extract_data.js

--- JavaScript basics/filter_extract_data.js
+++ JavaScript basics/filter_extract_data.js
@@ -117,3 +117,16 @@ const filteredList = watchList
   .map(({ Title: title, imdbRating: rating }) => ({ title, rating }));
 
 console.log(filteredList);
+
+// Only change code below this line
+Array.prototype.myFilter = function(callback) {
+const newArray = [];
+
+for(let i = 0; i < this.length; i++){
+if(callback(this[i], i , this)){
+newArray.push(this[i])
+}
+}
+// Only change code above this line
+return newArray;
+};
