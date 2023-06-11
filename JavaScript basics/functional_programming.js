7e72fff93e0ca2ea8610f00ba399303715316feb
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-11 18:39:04 +0300

    Update functional_programming.js

--- JavaScript basics/functional_programming.js
+++ JavaScript basics/functional_programming.js
@@ -189,3 +189,13 @@ function nonMutatingSplice(cities) {
   return cities.slice(0,3);
 }
 const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+  return original.concat(attach);
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
