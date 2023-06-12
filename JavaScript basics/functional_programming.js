db6d7c2cc6a169c3a8a7b8e1169e8d719e466845
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-12 22:02:09 +0300

    Update functional_programming.js

--- JavaScript basics/functional_programming.js
+++ JavaScript basics/functional_programming.js
@@ -201,6 +201,7 @@ const first = [1, 2, 3];
 const second = [4, 5];
 nonMutatingConcat(first, second);
 
+// Add Elements to the End of an Array Using concat Instead of push
 function nonMutatingPush(original, newItem) {
   // Only change code below this line
   let concatArray = original.concat(newItem);
