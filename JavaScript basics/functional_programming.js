1bd2643733f815697af4ed10373e4d557cce4882
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-12 21:49:00 +0300

    Update functional_programming.js

--- JavaScript basics/functional_programming.js
+++ JavaScript basics/functional_programming.js
@@ -200,3 +200,15 @@ function nonMutatingConcat(original, attach) {
 const first = [1, 2, 3];
 const second = [4, 5];
 nonMutatingConcat(first, second);
+
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  let concatArray = original.concat(newItem);
+   console.log(concatArray);
+   return concatArray
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
