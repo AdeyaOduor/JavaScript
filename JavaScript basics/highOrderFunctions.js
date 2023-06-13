7a53e80db3dfbcfde2f706db99a067cf327b8ab5
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-13 19:13:33 +0300

    Update highOrderFunctions.js

--- JavaScript basics/highOrderFunctions.js
+++ JavaScript basics/highOrderFunctions.js
@@ -1,1 +1,12 @@
+const squareList = arr => {
+  const positiveIntegers = arr.filter(num => {
+    return num >= 0 && Number.isInteger(num);
+  });
+  const squaredIntegers = positiveIntegers.map(num => {
+    return num ** 2;
+  });
+  return squaredIntegers;
+};
 
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
