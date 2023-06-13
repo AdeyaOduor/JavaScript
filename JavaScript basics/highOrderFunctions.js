b5b807f97d9de1f4895bdb600743a78213b2c344
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-13 19:15:05 +0300

    Update highOrderFunctions.js

--- JavaScript basics/highOrderFunctions.js
+++ JavaScript basics/highOrderFunctions.js
@@ -1,3 +1,4 @@
+// Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
 const squareList = arr => {
   const positiveIntegers = arr.filter(num => {
     return num >= 0 && Number.isInteger(num);
