f5acd2530f18a878d96b236a447e2c786acb331a
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-14 20:30:27 +0300

    Update functional_programming.js

--- JavaScript basics/functional_programming.js
+++ JavaScript basics/functional_programming.js
@@ -189,9 +189,8 @@ const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 
 // Combine Two Arrays Using the concat Method
 function nonMutatingConcat(original, attach) {
-  // Only change code below this line
+  
   return original.concat(attach);
-  // Only change code above this line
 }
 
 const first = [1, 2, 3];
