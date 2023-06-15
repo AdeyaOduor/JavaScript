ecf9f0cb706788f37195b155f22fd96a88b7cf4e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-15 21:09:27 +0300

    Update filter_list.js

--- Sort arrays/filter_list.js
+++ Sort arrays/filter_list.js
@@ -17,3 +17,11 @@ function firstNonConsecutive (arr) {
     }
     return null;
 }
+
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/\W/);
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
