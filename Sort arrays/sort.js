318d13bcd1c839b167176c2e77cccb80542b54e1
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-15 20:38:17 +0300

    Update sort.js

--- Sort arrays/sort.js
+++ Sort arrays/sort.js
@@ -80,3 +80,13 @@ function alphabeticalOrder(arr) {
   });
 }
 alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
+const globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Add your code below this line
+  return [].concat(arr).sort(function(a, b) {
+    return a - b;
+  });
+  // Add your code above this line
+}
+nonMutatingSort(globalArray);
