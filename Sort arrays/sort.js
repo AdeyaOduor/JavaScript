4ed60780fad32774f2c5bf8c81f3c59a03b63131
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-15 20:39:56 +0300

    Update sort.js

--- Sort arrays/sort.js
+++ Sort arrays/sort.js
@@ -84,10 +84,10 @@ alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
 // Return a Sorted Array Without Changing the Original Array
 const globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
-  // Add your code below this line
+  
   return [].concat(arr).sort(function(a, b) {
     return a - b;
   });
-  // Add your code above this line
+  
 }
 nonMutatingSort(globalArray);
