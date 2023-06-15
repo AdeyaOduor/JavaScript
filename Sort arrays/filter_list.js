00a88d85268b2ffe6067ee14088894793a30fc41
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-15 21:10:51 +0300

    Update filter_list.js

--- Sort arrays/filter_list.js
+++ Sort arrays/filter_list.js
@@ -20,9 +20,9 @@ function firstNonConsecutive (arr) {
 
 // Split a String into an Array Using the split Method
 function splitify(str) {
-  // Only change code below this line
+  
   return str.split(/\W/);
-  // Only change code above this line
+  
 }
 
 splitify("Hello World,I-am code");
