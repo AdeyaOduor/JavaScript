afe5be4887ba28eb884abc84a25f4813fc407373
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-16 15:24:04 +0300

    Update every_some_methods.js

--- JavaScript basics/every_some_methods.js
+++ JavaScript basics/every_some_methods.js
@@ -1,9 +1,15 @@
-// Use the every Method to Check that Every Element in an Array Meets a Criteria
+// The every Method to Check that Every Element in an Array Meets a Criteria
 function checkPositive(arr) {
   
   return arr.every(function(value) {
     return value > 0;
   });
   
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// The some Method to Check that Any Elements in an Array Meet a Criteria
+function checkPositive(arr) {
+  return arr.some(elem => elem > 0);
 }
 checkPositive([1, 2, 3, -4, 5]);
