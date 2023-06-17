0b40495609e4a1dad9c5bcec7fbe03d439c07df3
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-17 23:53:16 +0300

    Update diff_function.js

--- Sort arrays/diff_function.js
+++ Sort arrays/diff_function.js
@@ -2,3 +2,11 @@
 function array_diff(a, b) {
   return a.filter(function(x) { return b.indexOf(x) == -1; });
 }
+
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
