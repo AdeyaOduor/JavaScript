95b4d95e0d26b9bdf579da6b0fbccd0a6b3e0589
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-17 23:54:22 +0300

    Update diff_function.js

--- Sort arrays/diff_function.js
+++ Sort arrays/diff_function.js
@@ -3,6 +3,8 @@ function array_diff(a, b) {
   return a.filter(function(x) { return b.indexOf(x) == -1; });
 }
 
+/*Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, 
+return the symmetric difference of the two arrays.*/
 function diffArray(arr1, arr2) {
   return [...diff(arr1, arr2), ...diff(arr2, arr1)];
 
