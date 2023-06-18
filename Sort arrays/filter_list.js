a3a8283529d1755e188a1c9df8e7202f84c76081
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-18 20:38:16 +0300

    Update filter_list.js

--- Sort arrays/filter_list.js
+++ Sort arrays/filter_list.js
@@ -1,5 +1,4 @@
 //function taking a list of non-negative integers,strings and returns a new list with the strings filtered out.
-
 function filter_list(l) {
     // Return a new array with the strings filtered out
     var filt = l.filter(function(x) {
@@ -10,7 +9,6 @@ function filter_list(l) {
   }
 
 // find first non consecutive number in an array
-
 function firstNonConsecutive (arr) {
     for (let i = 1; i < arr.length; i++) {
         if (arr[i - 1] + 1 !== arr[i]) return arr[i];
