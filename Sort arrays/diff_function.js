c43967e095eb32aadabfed146c43d68b59dd0085
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-18 20:33:49 +0300

    Update diff_function.js

--- Sort arrays/diff_function.js
+++ Sort arrays/diff_function.js
@@ -20,3 +20,13 @@ function diffArray(arr1, arr2) {
 function destroyer(arr, ...valsToRemove) {
   return arr.filter(elem => !valsToRemove.includes(elem));
 }
+
+function whatIsInAName(collection, source) {
+  
+  const sourceKeys = Object.keys(source);
+
+  return collection
+    .filter(obj => sourceKeys
+      .every(key => obj.hasOwnProperty(key) &&
+        obj[key] === source[key]));
+}
