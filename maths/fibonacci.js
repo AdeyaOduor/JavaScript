50942d94725268dbb8a70a52dc474a735644e21d
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-19 21:22:12 +0300

    Update fibonacci.js

--- maths/fibonacci.js
+++ maths/fibonacci.js
@@ -3,6 +3,7 @@ let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) =>  previous + current);
 console.log(`Sum of array values is: ${arraySum}`); //output= Sum of array values is: 6
 
+// Sum All Odd Fibonacci Numbers
 function sumFibs(num) {
   let prevNumber = 0;
   let currNumber = 1;
