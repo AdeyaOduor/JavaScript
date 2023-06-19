278be070e4d74d5dbc48e6d8d24504128fae186d
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-19 21:03:24 +0300

    Update conditionals.js

--- JavaScript basics/conditionals.js
+++ JavaScript basics/conditionals.js
@@ -302,5 +302,5 @@ function abTest(a, b) {
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change values below to test your code
-abTest(2, 2);
+abTest(2, 2); // Change values below to test your code
+
