4b4f5df42e666e5fa592709f22d6257bf7d7a627
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-22 23:42:34 +0300

    Update filter_extract_data.js

--- JavaScript basics/filter_extract_data.js
+++ JavaScript basics/filter_extract_data.js
@@ -136,3 +136,33 @@ function sliceArray(anim, beginSlice, endSlice) {
   return anim.slice(beginSlice, endSlice);
 }
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
+/* Given the array arr, iterate through and remove each element 
+starting from the first element (the 0 index) until the function 
+ returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, 
+otherwise, arr should be returned as an empty array.
+
+Cannot use filter, since we need to return the rest of the array
+once the condition is met. We use a 'For' loop ! */
+
+function dropElements(arr, func) {
+  // Drop them elements.
+  let myArr=[];
+  for (let i=0; i<arr.length; i++) {
+    if (func(arr[i])) {
+      myArr=arr.slice(i);
+      break;
+    }
+  }
+  return myArr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; }); // [1,2]
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) // [3,4]
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}); // [1, 0, 1]
+dropElements([1, 2, 3], function(n) {return n > 0;}); // [1, 2, 3]
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}); // []
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}); // [7, 4]
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}); // [3, 9, 2]
