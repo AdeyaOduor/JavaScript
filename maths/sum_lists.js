5d7c43a13e97d44c4cc1d49a0bf89c20cb89ea1e
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-25 00:40:28 +0300

    Update sum_lists.js

--- maths/sum_lists.js
+++ maths/sum_lists.js
@@ -93,3 +93,16 @@ e.next = f;
 var newHead = sumList(a, d);
 
 printList(newHead);
+
+/*Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.*/
+
+const addTogether = ( a, ...arr ) =>
+  typeof a !== "number" ? undefined
+  : arr.length === 0 ? addT( a )
+  : addT( a )( arr[0] )
+
+const addT = a => b =>
+  typeof b !== "number"  ? undefined
+  : a + b
