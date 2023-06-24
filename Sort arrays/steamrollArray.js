198d0b28d715a2318ead2130c8546d7a5387d93d
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-24 23:05:17 +0300

    Update steamrollArray.js

--- Sort arrays/steamrollArray.js
+++ Sort arrays/steamrollArray.js
@@ -1,1 +1,14 @@
+function steamrollArray(arr) {
+  let flattened = [];
 
+  arr.map(val => {
+    if (!Array.isArray(val)) {
+      flattened.push(val);
+    }
+    else {
+      flattened.push(...steamrollArray(val));
+    }
+  })
+
+  return flattened;
+}
