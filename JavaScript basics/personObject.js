d2f22c8f5ba113b0b897481248dfbb1cda91980d
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-06-25 00:41:57 +0300

    Update personObject.js

--- JavaScript basics/personObject.js
+++ JavaScript basics/personObject.js
@@ -1,1 +1,36 @@
+// Create person object
+const Person = function(firstAndLast) {
+  
+
+  let [firstName, lastName] = firstAndLast.split(' ');
+
+  this.getFirstName = function() {
+    return firstName;
+  };
+
+  this.getLastName = function() {
+    return lastName;
+  };
+
+  this.getFullName = function() {
+    return `${firstName} ${lastName}`;
+  };
+
+  this.setFirstName = function(first) {
+    firstName = first;
+    return firstName;
+  };
+
+  this.setLastName = function(last) {
+    lastName = last;
+    return lastName;
+  }
+
+  this.setFullName = function(firstAndLast) {
+    firstName = firstAndLast.split(' ')[0];
+    lastName = firstAndLast.split(' ')[1];
+    return `${firstName} ${lastName}`;
+  }
+
+};
 
