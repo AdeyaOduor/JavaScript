07d92bda6fbf5b013a44d4920dc77b02b3bbebca
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2024-01-28 19:09:44 +0300

    Update mean_score_grading.js

--- maths/mean_score_grading.js
+++ maths/mean_score_grading.js
@@ -3,28 +3,26 @@
 let students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
 
 let Avgmarks = 0;
+let avg = 0;
 
-for (let i=0; i < students.length; i++) {
-        Avgmarks += students[i][1];
-        let avg = (Avgmarks/students.length);
+for (let i = 0; i < students.length; i++) {
+    Avgmarks += students[i][1];
 }
 
-console.log("Average grade: " + (Avgmarks)/students.length);
+avg = Avgmarks / students.length;
+console.log("Average grade: " + avg);
 
-        if (avg < 40){
-          console.log("Grade : F");      
-          } 
-        else if (avg < 50) {
-                console.log("Grade : D"); 
-                  } 
-        else if (avg < 60) 
-             {
-                console.log("Grade : C"); 
-        } else if (avg < 80) {
-                console.log("Grade : B"); 
-        } else if (avg < 100) {
-                console.log("Grade : A")
-        };
+if (avg < 40) {
+    console.log("Grade : F");
+} else if (avg < 50) {
+    console.log("Grade : D");
+} else if (avg < 60) {
+    console.log("Grade : C");
+} else if (avg < 80) {
+    console.log("Grade : B");
+} else if (avg < 100) {
+    console.log("Grade : A");
+}
 
 // method2
 function grades(students) {
