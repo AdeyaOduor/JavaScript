14db5e5a4f718b33af62ef7e9b011281606ec7ff
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-03-17 22:20:54 +0300

    Update binaryAgents.js

--- JavaScript basics/binaryAgents.js
+++ JavaScript basics/binaryAgents.js
@@ -8,3 +8,6 @@ function binaryAgent(str) {
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 
             01100010 01101111 01101110 01100110 01101001 01110010 01100101 
   01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+// The binary values in the input string are converted into their respective ASCII characters, resulting in the following text string.
+//
