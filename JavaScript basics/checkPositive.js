a358929e06463d6614d61820cdcae55a6e07d8c9
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-03-17 22:16:36 +0300

    Update checkPositive.js

--- JavaScript basics/checkPositive.js
+++ JavaScript basics/checkPositive.js
@@ -6,11 +6,12 @@ function checkPositive(arr) {
   });
   
 }
-checkPositive([1, 2, 3, -4, 5]);
+checkPositive([1, 2, 3, -4, 5]); // The output will be false because the array [1, 2, 3, -4, 5] contains the value -4, which is not greater than 0.
 
-// The same Method to Check that Any Elements in an Array Meet a Criteria
+// The some Method to Check that Any Elements in an Array Meet a Criteria
 function checkPositive(arr) {
   return arr.some(elem => elem > 0); 
 }
 checkPositive([1, 2, 3, -4, 5]);
-// The output will be false because the array [1, 2, 3, -4, 5] contains the value -4, which is not greater than 0.
+// The output will be true because the array [1, 2, 3, -4, 5] contains values that are greater than 0 (1, 2, 3, and 5).
+
