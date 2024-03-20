f28665fdee3c27a6e1829d0eded54c757d7c1252
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-03-20 19:38:22 +0300

    Update urlify.js

--- URLify/urlify.js
+++ URLify/urlify.js
@@ -21,4 +21,27 @@ var urlify = function(str, length) {
 };
 
 console.log(urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith'); // output Mr%20John%20i Mr%20John%20Smith
+---------------------------------------------------------------------------------------------------------------------------
+//OR
+
+function urlify(str, len) {
+  let s = "";
+  let totalSpaces = str.length - len;
+  let frontSpaces = 0;
+  let flag = false;
+  for (let i = 0; i < str.length; i++) {
+    if (flag === false) {
+      if (str[i] === " ") frontSpaces++;
+      else flag = true;
+    }
+    if (flag === true && i < str.length - (totalSpaces - frontSpaces)) {
+      if (str[i] === " ") s += "%20";
+      else s += str[i];
+    }
+  }
+
+  return s;
+}
+
+console.log(urlify("Mr John Smith   ", 13));
 
