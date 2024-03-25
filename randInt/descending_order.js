0079bec19b31958ffd70b99a0241d809b1bd8c25
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-03-25 18:41:10 +0300

    Update descending_order.js

--- randInt/descending_order.js
+++ randInt/descending_order.js
@@ -1,10 +1,13 @@
 function descendingOrder() {
-   let num= 209482;
-   let arr_num = array.from(num + "");
-   let arr_num = num_arr.map(e=> parseInt(e));
+   let num = 209482;
+   let arr_num = Array.from(num + "");
+   arr_num = arr_num.map(e => parseInt(e));
    arr_num.sort();
    arr_num.reverse();
-   arr_num.sort(a,b) => a<b);
-   let str_arr = arr_num.join;
+   arr_num.sort((a, b) => a - b);
+   let str_arr = arr_num.join("");
 
+   return str_arr;
 }
+
+console.log(descendingOrder());
