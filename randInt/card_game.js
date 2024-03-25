4b03ff067d6bbc24fcde48e3c431b9774d5581ef
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-03-25 18:43:43 +0300

    Update card_game.js

--- randInt/card_game.js
+++ randInt/card_game.js
@@ -2,15 +2,23 @@
 let count = 0;
 
 function cc(card) {
-  
   var regex = /[JQKA]/;
-  if (card > 1 && card < 7) {
+
+  if (card >= 2 && card <= 6) {
     count++;
   } else if (card === 10 || regex.test(card)) {
     count--;
   }
 
-  if (count > 0) return count + " Bet";
-  return count + " Hold";
-
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
 }
+
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc('K'));
+console.log(cc('A'));
