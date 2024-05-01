8a73a5649060bfe5d639b595db0fb63d44663a9e
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-01 20:32:26 +0300

    Update functional_programming.js

--- JavaScript basics/functional_programming.js
+++ JavaScript basics/functional_programming.js
@@ -56,9 +56,9 @@ function nextInLine(arr, item) {
  
 var testArr = [1,2,3,4,5];
  
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
+console.log("Before: " + JSON.stringify(testArr)); // Before: [1,2,3,4,5]
+console.log(nextInLine(testArr, 6)); //1
+console.log("After: " + JSON.stringify(testArr)); // After: [2,3,4,5,6]
 -----------------------------------------------------------------------------------------------------------------
 // strict equality
 function testStrict(val) {
