7243efc555b0ce2bc1afccf7bde895e32ffc986c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-06 22:57:39 +0300

    Update highOrderFunctions.js

--- JavaScript basics/highOrderFunctions.js
+++ JavaScript basics/highOrderFunctions.js
@@ -1,4 +1,6 @@
 // Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+/* A function named squareList that takes an array (arr) as input. The function's purpose is to filter the input array for positive integers, 
+square each of the filtered integers, and return the resulting array of squared integers.*/
 const squareList = arr => {
   const positiveIntegers = arr.filter(num => {
     return num >= 0 && Number.isInteger(num);
