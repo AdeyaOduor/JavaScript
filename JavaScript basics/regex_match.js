5a3d353dc4aeb869b50e696aa7c99b33ccb39afa
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-22 10:54:42 +0300

    Update regex_match.js

--- JavaScript basics/regex_match.js
+++ JavaScript basics/regex_match.js
@@ -24,6 +24,8 @@ let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
 
+console.log(result); 
+-----------------------------------------------------------------------------------------------
 let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
