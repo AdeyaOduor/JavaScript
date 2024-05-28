98e6a4a4e5804006d8ef2decb7c753d65c34be7b
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-29 00:00:26 +0300

    Update regex_match.js

--- JavaScript basics/regex_match.js
+++ JavaScript basics/regex_match.js
@@ -78,6 +78,7 @@ console.log(result); // 31
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g; // Change this line
 let result = quoteSample.match(nonAlphabetRegex).length;
+console.log(result); //6
 ------------------------------------------------------------------------------------------------
 // match-all-numbers
 
