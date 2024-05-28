61b68b73ae7a565a11eb4c056284f9327cbd4186
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-29 00:02:17 +0300

    Update regex_match.js

--- JavaScript basics/regex_match.js
+++ JavaScript basics/regex_match.js
@@ -85,6 +85,8 @@ console.log(result); //6
 let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
+console.log(result); //4
+-------------------------------------------------------------------------------------------------
 
 // match-all-non-numbers
 
