567bd145681e882ede9d292334226e2848250443
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-01 01:05:03 +0300

    Update regex_test.js

--- JavaScript basics/regex_test.js
+++ JavaScript basics/regex_test.js
@@ -33,7 +33,8 @@ console.log(result); // true
 let username = "JackOfAllTrades";
 const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
 let result = userCheck.test(username);
-
+console.log(result); // true
+--------------------------------------------------------------------------------
 // positive-and-negative-lookahead
 
 let sampleWord = "astronaut";
