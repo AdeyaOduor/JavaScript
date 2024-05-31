984cefa1891b4f2a5ae639856d76716171c96a26
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-05-31 07:40:53 +0300

    Update variables.js

--- JavaScript basics/variables.js
+++ JavaScript basics/variables.js
@@ -1,9 +1,6 @@
-/* When JavaScript variables are declared, they have an initial value of undefined. A mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.*/
-
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+/* When JavaScript variables are declared, they have an initial value of undefined. 
+A mathematical operation on an undefined variable your result will be NaN which means "Not a Number". 
+If you concatenate a string with an undefined variable, you will get a string of undefined.*/
 
 // Variable assignments
 studlyCapVar = 10;
