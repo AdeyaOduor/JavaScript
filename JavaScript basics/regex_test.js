0ad76b7d68080edac8c8358ca8b97443171c13b6
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-01 01:05:46 +0300

    Update regex_test.js

--- JavaScript basics/regex_test.js
+++ JavaScript basics/regex_test.js
@@ -40,7 +40,7 @@ console.log(result); // true
 let sampleWord = "astronaut";
 let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
 let result = pwRegex.test(sampleWord);
-
+----------------------------------------------------------------------------------
 /* function to check whether a given value is an valid url or not */
 
 function is_url(str)
