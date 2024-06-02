8d1424c0e8c5b60dac071e50557326174e9c6ea0
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-03 00:10:11 +0300

    Update median.js

--- maths/median.js
+++ maths/median.js
@@ -8,5 +8,5 @@ const median = arr => {
     nums = [...arr].sort((a, b) => a - b);
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
-console.log(median([5, 6, 50, 1, -5]));
-console.log(median([1, 2, 3, 4, 5]));
+console.log(median([5, 6, 50, 1, -5])); //5
+console.log(median([1, 2, 3, 4, 5]));//3
