f66d1abb1a63e3718ebe3d5fe368ab11b81e7af7
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-11 01:22:15 +0300

    Update strings_to_url.js

--- JavaScript basics/strings_to_url.js
+++ JavaScript basics/strings_to_url.js
@@ -7,4 +7,4 @@ function urlSlug(title) {
     .toLowerCase();
 }
 
-console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone")); // a-mind-needs-books-like-a-sword-needs-a-whetstone
