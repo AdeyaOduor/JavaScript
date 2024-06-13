7f0ad5c62f6fb742d3a04632c7825f99a43c1af3
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-13 23:21:18 +0300

    Update and rename non_consecutive_number.js to non.js

--- Sort arrays/non_consecutive_number.js
+++
@@ -1,8 +0,0 @@
-/* find the first non consecutive number */
-
-function firstNonConsecutive(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i - 1] + 1 !== arr[i]) return arr[i];/
-    }
-    return null;
-}
