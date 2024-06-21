33abfa9c2329f3c80af566d407beb3b5d5030c78
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-22 00:21:15 +0300

    Update standard_deviation.js

--- maths/standard_deviation.js
+++ maths/standard_deviation.js
@@ -9,3 +9,21 @@ const standardDeviation = (arr, usePopulation = false) => {
 
 console.log(standardDeviation([10, 2, 38, 23, 38, 23, 21])); // 13.284434142114991
 console.log(standardDeviation([10, 2, 38, 23, 38, 23, 21], true)); // 12.29899614287479
+-------------------------------------------------------------------------------------------------------------------
+//program to get the variance of an array of numbers.
+  
+  function calculateVariance(arr) {
+  // Calculate the mean of the array
+  const mean = arr.reduce((sum, val) => sum + val, 0) / arr.length;
+
+  // Calculate the squared differences from the mean
+  const squaredDifferences = arr.map(val => Math.pow(val - mean, 2));
+
+  // Calculate the variance
+  const variance = squaredDifferences.reduce((sum, val) => sum + val, 0) / arr.length;
+
+  return variance;
+}
+
+console.log(calculateVariance([10, 2, 38, 23, 38, 23, 21])); // 151.26530612244895
+// console.log(calculateVariance([10, 2, 38, 23, 38, 23, 21], true)); // 151.26530612244895
