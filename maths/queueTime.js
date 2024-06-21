126a57a11498acc0d99f3a45c45950b732e7dd77
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-21 23:48:06 +0300

    Update queueTime.js

--- maths/queueTime.js
+++ maths/queueTime.js
@@ -1,5 +1,5 @@
-// There is a queue for the self-checkout tills at the supermarket. 
-// Your task is write a function to calculate the total time required for all the customers to check out!
+/* There is a queue for the self-checkout tills at the supermarket. 
+ Your task is write a function to calculate the total time required for all the customers to check out!*/
 
 function queueTime(customers, n) {
   return Math.max(...customers.reduce((acc, c) => { let i = acc.indexOf(Math.min(...acc)); acc[i] = acc[i] + c;  return acc; },Array(n).fill(0)))
