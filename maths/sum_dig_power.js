48f8f6f4b923dfc9e2aceb2c554bae389da0ae55
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-06-29 22:50:12 +0300

    Update sum_dig_power.js

--- maths/sum_dig_power.js
+++ maths/sum_dig_power.js
@@ -28,6 +28,8 @@ function sumDigPow(a, b) {
   return l;
 }
 
+console.log(sumDigPow(1, 100));
+
 /*
     The function sumDigPow takes two parameters, a and b, which represent the range of numbers to be checked.
     An empty array l is initialized to store the numbers that satisfy the condition.
@@ -41,6 +43,7 @@ function sumDigPow(a, b) {
     If the condition is true, the current number i is added to the l array.
     Finally, the function returns the l array containing the numbers that satisfy the condition.
 
-The purpose of this function is to find all the numbers in the given range [a, b] where the sum of the digits raised to their respective positions is equal to the original number. These numbers are often referred to as "narcissistic" or "Armstrong" numbers.
-
-For example, if you call sumDigPow(1, 100), the function will return an array containing the numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 89], as these are the only numbers in the range [1, 100] that satisfy the given condition.*/
+The purpose of this function is to find all the numbers in the given range [a, b] where the sum of the digits raised to their respective positions is 
+equal to the original number. These numbers are often referred to as "narcissistic" or "Armstrong" numbers.
+For example, if you call sumDigPow(1, 100), the function will return an array containing the numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 89], as these are the 
+only numbers in the range [1, 100] that satisfy the given condition.*/
