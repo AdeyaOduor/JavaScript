2a07a831720241c5ceeec1abed49ff82307dbb9c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-07-05 16:39:48 +0300

    Update multiples_of_integers.js

--- maths/multiples_of_integers.js
+++ maths/multiples_of_integers.js
@@ -8,4 +8,36 @@ console.log(num); /* [
    72,  81,  90,  99, 108, 117, 126,
   135, 144, 153, 162, 171, 180, 189,
   198, 207, 216, 225, 234, 243
-]*/
+]
+
+Real World Applications include:
+    Scheduling and Timetabling:
+        In scheduling applications, such as school or work timetables, the array of numbers could represent time slots in a day, 
+        with each increment of 9 representing a 15-minute interval (assuming a standard 9-to-5 workday).
+        This could be used to efficiently generate a list of available time slots for scheduling meetings, classes, or other events.
+
+    Inventory Management:
+        In a retail or manufacturing setting, the array of numbers could represent the quantities of a particular product or component 
+        that need to be maintained in inventory.
+        By using the array, you can easily track and manage the inventory levels, ensuring that you have the right amounts at the right 
+        times.
+
+    Numerical Simulations:
+        In scientific or engineering applications, the array of numbers could represent a range of input values or parameters for a numerical 
+        simulation or model.
+        For example, in a fluid dynamics simulation, the array could represent different flow rates or pressure values that need to be tested.
+
+    Pricing and Discounting:
+        In the context of pricing or discounting, the array of numbers could represent the different price points or discount levels that a 
+        business might offer for a product or service.
+        This could be useful for implementing dynamic pricing strategies or for automating the process of generating pricing tables.
+
+    Time Series Analysis:
+        In the field of time series analysis, the array of numbers could represent a sequence of time points or intervals, such as months or 
+        quarters, that need to be analyzed.
+        This could be useful for tasks like forecasting, trend analysis, or identifying seasonal patterns in data.
+
+    Tax Calculations:
+        In the realm of taxation, the array of numbers could represent different tax brackets or income thresholds that need to be considered when calculating tax liabilities.
+        This could help streamline the process of computing taxes for individuals or businesses.
+*/
