5ddf03106f3d1f02772f2ef5a19584ce193fdee0
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-07-22 23:28:46 +0300

    Update median.js

--- maths/median.js
+++ maths/median.js
@@ -1,6 +1,7 @@
-// Find the middle of the array, use Array.sort() to sort the values. 
-// Return the number at the midpoint if length is odd, 
-//otherwise the average of the two middle numbers.
+/* Find the middle of the array, use Array.sort() to sort the values. 
+ Return the number at the midpoint if length is odd, otherwise the average of the two middle numbers.
+ 
+ */
 
 
 const median = arr => {
