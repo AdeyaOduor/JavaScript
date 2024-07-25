4699eec6a0b2f8d08680dd13fbcb2431afcc2f88
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-07-25 23:11:20 +0300

    Update formatedString.js

--- stings/formatedString.js
+++ stings/formatedString.js
@@ -1,5 +1,5 @@
-//Given: an array containing hashes of names
-//Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+/* Given: an array containing hashes of names, return a string formatted as a list of names separated by 
+commas except for the last two names, which should be separated by an ampersand. */
 
 function list(names) {
     let len = names.length;
