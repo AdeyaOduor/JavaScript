25e9dd3a8db37b293748fb8d7f0cfc0d73d66f78
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-07-26 00:12:36 +0300

    Update camelCase.js

--- stings/camelCase.js
+++ stings/camelCase.js
@@ -1,5 +1,5 @@
-  //Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
-  //The first word within the output should be capitalized only if the original word was capitalized 
+  /* Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first 
+  word within the output should be capitalized only if the original word was capitalized. */ 
 
   function toCamelCase(str){
     var regExp=/[-_]\w/ig;
