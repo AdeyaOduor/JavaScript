72a5f1040ed57517cb634b6c5ba6227bd3668331
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-07-29 23:49:18 +0300

    Update steamrollArray.js

--- Sort arrays/steamrollArray.js
+++ Sort arrays/steamrollArray.js
@@ -13,3 +13,8 @@ function steamrollArray(arr) {
 
   return flattened;
 }
+// This could be used to prepare data for further processing or analysis, where a flat data structure is required.
+const nestedData = [1, [2, [3, 4], 5], [6, [7, 8]]];
+const flatData = steamrollArray(nestedData); // flatData = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
