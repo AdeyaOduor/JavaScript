b1029d4e6e5414fc8a51329e587fdf51f2611b49
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-17 18:24:50 +0300

    Update validIp.js

--- stings/validIp.js
+++ stings/validIp.js
@@ -1,3 +1,6 @@
+// The isValidIP function checks if a given string is a valid IPv4 address. Validating IP addresses is 
+// crucial in various real-world applications, particularly in networking, web development, and cybersecurity.
+
 function isValidIP(str) {
     return str.split('.').filter(function(v){return v==Number(v).toString() && Number(v)<256}).length==4;
   }
