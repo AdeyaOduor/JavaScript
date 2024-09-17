1282679ac5ace6fa45a587bfbb1aa5c849ce70b3
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-17 18:31:26 +0300

    Update validIp.js

--- stings/validIp.js
+++ stings/validIp.js
@@ -5,7 +5,7 @@ function isValidIP(str) {
     return str.split('.').filter(function(v){return v==Number(v).toString() && Number(v)<256}).length==4;
   }
 -----------------------------------------------------------------------------------------------------------------
-
+// Example
     <!DOCTYPE html>
 <html lang="en">
 <head>
