f0608e1c8c300196691c459c8d8fa00d67073ff4
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-20 01:18:49 +0300

    Update incrementStrings.js

--- stings/incrementStrings.js
+++ stings/incrementStrings.js
@@ -1,6 +1,9 @@
- /* Your job is to write a function which increments a string, to create a new string.
-    If the string already ends with a number, the number should be incremented by 1.
-    If the string does not end with a number. the number 1 should be appended to the new string. */
+ /*
+ The incrementString function is useful for incrementing numeric suffixes in strings, 
+ which is particularly applicable in various real-world scenarios, such as file naming, versioning, and user-generated content. 
+ 
+ incrementString function  increments a string, to create a new string. If the string already ends with a number, the number should be incremented by 1.
+ If the string does not end with a number. the number 1 should be appended to the new string. */
 
 function incrementString (strng) {
     // return incrementedString
