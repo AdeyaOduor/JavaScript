eaf47cdd64cf964ef2241bb9686c9f2d2252b317
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-20 01:10:27 +0300

    Update repeatString.js

--- stings/repeatString.js
+++ stings/repeatString.js
@@ -1,3 +1,7 @@
+/*
+The repeatStringNumTimes function recursively repeats a given string a specified number of times. 
+This functionality has several practical applications in programming, particularly in scenarios where string manipulation is required. */
+
 function repeatStringNumTimes(str, num) {
   return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
 }
