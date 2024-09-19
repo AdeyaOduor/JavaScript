f1ba7599d13d103f72cf35c644875ef08cd5bbb0
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-19 23:14:18 +0300

    Update titleCase.js

--- stings/titleCase.js
+++ stings/titleCase.js
@@ -1,5 +1,8 @@
 /* Return the provided string with the first letter of each word capitalized. 
-Make sure the rest of the word is in lower case.*/
+Make sure the rest of the word is in lower case.
+
+The titleCase function is useful for formatting strings to a title case, where the first letter of each word is capitalized. 
+This functionality can be applied in various real-world scenarios, particularly in data presentation and user interfaces.*/
 function titleCase(str) {
   return str
     .toLowerCase()
