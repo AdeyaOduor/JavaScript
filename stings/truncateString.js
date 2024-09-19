5dc7d55ceebce908a74ed25204a5180bd549cb4c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-19 23:08:43 +0300

    Update truncateString.js

--- stings/truncateString.js
+++ stings/truncateString.js
@@ -1,5 +1,7 @@
 /* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
-Return the truncated string with a ... ending. */
+Return the truncated string with a ... ending. 
+
+The truncateString function is useful for shortening long strings, particularly in user interfaces and data processing scenarios.*/
 function truncateString(str, num) {
   return str.length > num ? str.slice(0, num) + "..." : str;
 }
