8755dd12d218a73b203af3116ee5ffcde384ab5c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-21 18:39:48 +0300

    Update confirmEnding.js

--- stings/confirmEnding.js
+++ stings/confirmEnding.js
@@ -1,3 +1,8 @@
+/*
+The confirmEnding function checks if a given string ends with a specified target string. 
+This functionality has several practical applications in real-world scenarios, particularly in text processing, 
+user input validation, and file handling. */
+
 function confirmEnding(str, target) {
   return str.slice(-target.length) === target
 }
