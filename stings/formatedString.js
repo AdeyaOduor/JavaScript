1b0913e0d6935faa049d0a5d96d51538aaac9eff
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-21 18:34:16 +0300

    Update formatedString.js

--- stings/formatedString.js
+++ stings/formatedString.js
@@ -1,5 +1,8 @@
 /* Given: an array containing hashes of names, return a string formatted as a list of names separated by 
-commas except for the last two names, which should be separated by an ampersand. */
+commas except for the last two names, which should be separated by an ampersand. 
+
+The list function is designed to format an array of names into a readable string, making it particularly 
+useful in various real-world applications, such as displaying user lists, participant names, or any collection of entities. */
 
 function list(names) {
     let len = names.length;
