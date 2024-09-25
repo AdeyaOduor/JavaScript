c2a750698daa1922f58b60ebfcde589d5b1c443c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-09-26 00:49:59 +0300

    Update random_hex_color_code.js

--- randInt/random_hex_color_code.js
+++ randInt/random_hex_color_code.js
@@ -1,7 +1,10 @@
 /* program to generate a random hexadecimal color code.
 
 The random_hex_color_code function generates random hexadecimal color codes, 
-which can be applied in various real-world applications, especially in web development, graphic design, and data visualization. */
+which can be applied in various real-world applications, especially in web development, graphic design, and data visualization.
+
+Use Math.random() to generate a random 24-bit (6 * 4bits) hexadecimal number.
+Use bit shifting and then convert it to an hexadecimal string using Number.prototype.toString(16).*/
 
 const random_hex_color_code = () => {
   let n = (Math.random() * 0xfffff * 1000000).toString(16);
@@ -10,5 +13,5 @@ const random_hex_color_code = () => {
 
 console.log(random_hex_color_code())
 
-/* Use Math.random() to generate a random 24-bit (6 * 4bits) hexadecimal number.
-Use bit shifting and then convert it to an hexadecimal string using Number.prototype.toString(16). */
+//Example 1: In web apps to change the background color dynamically for aesthetic purposes or to enhance user experience.
+document.body.style.backgroundColor = random_hex_color_code();
