fc3c6d4098c7cbbbba6da7d7947fbf1144825026
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-11-26 00:10:13 +0300

    Update hashtagGenerator.js

--- codewarPractice/hashtagGenerator.js
+++ codewarPractice/hashtagGenerator.js
@@ -1,4 +1,9 @@
-function generateHashtag (str) {
-  if (str.length == 0 || str.length > 140) return false;
-  return '#' + str.split(' ').filter(function(x){return x.length != 0;}).map(function(x){return x[0].toUpperCase() + x.slice(1);}).join('');
+function generateHashtag(str) {
+  if (str.length == 0 || str.length > 140) return false; // Check if the string is empty or too long
+  return '#' + str.split(' ') // Split the string into words
+    .filter(function(x) { return x.length != 0; }) // Remove any empty words
+    .map(function(x) { // Capitalize the first letter of each word
+      return x[0].toUpperCase() + x.slice(1);
+    })
+    .join(''); // Join the words back together
 }
