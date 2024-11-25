f8bf91214baa9077d823fd9b70502d94f2a3ad88
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-11-25 23:27:39 +0300

    Update convert_int32_to_Ip.js

--- codewarPractice/convert_int32_to_Ip.js
+++ codewarPractice/convert_int32_to_Ip.js
@@ -1,6 +1,6 @@
 const int32ToIp = (num) => {
-   return (num >>> 24 & 0xFF) + '.' +
-   (num >>> 16 & 0xFF) + '.' +
-   (num >>> 8 & 0xFF) + '.' +
-   (num & 0xFF);
+   return (num >>> 24 & 0xFF) + '.' +  // Extract the first octet
+          (num >>> 16 & 0xFF) + '.' +  // Extract the second octet
+          (num >>> 8 & 0xFF) + '.' +   // Extract the third octet
+          (num & 0xFF);                 // Extract the fourth octet
 };
