b5fa3bbe7b8ac1ed8ddeeda83bc0c32839960ba4
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-07 00:58:20 +0300

    Update parking_lot.js

--- codewarPractice/parking_lot.js
+++ codewarPractice/parking_lot.js
@@ -70,3 +70,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Test the post and get methods for park and exit fuctions endpoints.
