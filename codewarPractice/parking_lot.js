95ad47659d3b71139e96d5994fb94b060b33eaf5
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-06 23:05:19 +0300

    Update parking_lot.js

--- codewarPractice/parking_lot.js
+++ codewarPractice/parking_lot.js
@@ -1,33 +1,71 @@
-var ParkingLot = function(spaces) {
-  this.limit = spaces;
-  this.number = 0;
-  this.cars = {};
-};
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-ParkingLot.prototype.park = function(car) {
-  if (this.number <= this.limit) {
-    if (this.cars[car.name] !== undefined) {
-      console.log('car is already inside parking lot');
+app.use(bodyParser.json()); // Middleware to parse JSON bodies
+
+// ParkingLot class definition
+class ParkingLot {
+  constructor(spaces) {
+    this.limit = spaces;
+    this.number = 0;
+    this.cars = {};
+  }
+
+  park(car) {
+    if (this.number < this.limit) {
+      if (this.cars[car.name] !== undefined) {
+        return 'Car is already inside parking lot';
+      } else {
+        this.cars[car.name] = car;
+        this.number++;
+        return 'Car parked successfully';
+      }
     } else {
-      this.cars[car.name] = car;
-      this.number++;      
+      return 'The parking lot is full!';
     }
-  } else {
-    console.log('the parking lot is full!');
   }
-};
 
-ParkingLot.prototype.exit = function(car) {
-  if (this.number === 0) {
-    console.log('there are no cars in the parking lot');
-  } else if (this.cars[car.name] === undefined) {
-    console.log('the car is not in the parking lot');
-  } else {
-    delete this.cars[car.name];
-    this.number--;
+  exit(car) {
+    if (this.number === 0) {
+      return 'There are no cars in the parking lot';
+    } else if (this.cars[car.name] === undefined) {
+      return 'The car is not in the parking lot';
+    } else {
+      delete this.cars[car.name];
+      this.number--;
+      return 'Car exited successfully';
+    }
   }
-};
 
-ParkingLot.prototype.available = function() {
-  return this.number < this.limit;
-};
+  available() {
+    return this.number < this.limit;
+  }
+}
+
+// Create a ParkingLot instance with a limit of 5 spaces
+const parkingLot = new ParkingLot(5);
+
+// API endpoints
+app.post('/park', (req, res) => {
+  const car = req.body; // Expecting { name: 'CarName' }
+  const message = parkingLot.park(car);
+  res.send(message);
+});
+
+app.post('/exit', (req, res) => {
+  const car = req.body; // Expecting { name: 'CarName' }
+  const message = parkingLot.exit(car);
+  res.send(message);
+});
+
+app.get('/available', (req, res) => {
+  const isAvailable = parkingLot.available();
+  res.send({ available: isAvailable });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
