4ddcc6aa0ca98fb33f67704284d9292283b46f87
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-08 19:31:13 +0300

    Update checkPositive.js

--- JavaScript basics/checkPositive.js
+++ JavaScript basics/checkPositive.js
@@ -1,11 +1,11 @@
 // The every Method to Check that Every Element in an Array Meets a Criteria
 function checkPositive(arr) {
   const result = arr.every(function(value) {
-    return value > 0;
+    return value > 0; // Check if each value is greater than 0
   });
 
-  console.log(result);
-  return result;
+  console.log(result); // Log the result (true or false)
+  return result; // Return the result
 }
 
 checkPositive([1, 2, 3, -4, 5]);// The output will be false because the array [1, 2, 3, -4, 5] contains the value -4, which is not greater than 0.
