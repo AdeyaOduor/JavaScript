db89ad30fcd94e4405e3fcd44662b5fa1533bb62
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-11 00:10:53 +0300

    Update csv_to_array.js

--- maths/csv_to_array.js
+++ maths/csv_to_array.js
@@ -1,16 +1,10 @@
 //program to convert a comma-separated values (CSV) string to a 2D array
 const csv_to_array = (data, delimiter = ',', omitFirstRow = false) =>
   data
-    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
-    .split('\n')
-    .map(v => v.split(delimiter));
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0) // Remove the first row if specified
+    .split('\n') // Split the data into rows
+    .map(v => v.split(delimiter)); // Split each row into columns
 
-console.log(csv_to_array('a,b\nc,d')); 
-console.log(csv_to_array('a;b\nc;d', ';')); 
-console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
-/* [ [ 'a', 'b' ], [ 'c', 'd' ] ]
-[ [ 'a', 'b' ], [ 'c', 'd' ] ]
-[ [ 'a', 'b' ], [ 'c', 'd' ] ]*/
 
 const salesData = csv_to_array(fetchCSVData('sales_data.csv'), ',', true);
 // Analyze the sales data and generate visualizations
