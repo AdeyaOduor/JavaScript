8e65c784aa12d28a496672b3a94567a77b5231cd
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-12 00:47:51 +0300

    Update fibonacci.js

--- maths/fibonacci.js
+++ maths/fibonacci.js
@@ -2,7 +2,7 @@
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) =>  previous + current);
 console.log(`Sum of array values is: ${arraySum}`); //output= Sum of array values is: 6
---------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
 // Sum All Odd Fibonacci Numbers
 function sumFibs(num) {
   let prevNumber = 0;
@@ -21,7 +21,7 @@ function sumFibs(num) {
 
 // sumFibs(4);
 console.log(sumFibs(4)); //5
-----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------
   /*
   One application of the Fibonacci sequence is in the field of technical analysis for stock price prediction. 
   Technical analysts often use Fibonacci retracement levels to identify potential support and resistance levels in stock prices.
