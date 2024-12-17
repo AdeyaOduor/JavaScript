50e0cb458e00daa12572bde04c967307a21c197d
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2024-12-17 23:42:07 +0300

    Update remove_list_duplicates.js

--- codewarPractice/remove_list_duplicates.js
+++ codewarPractice/remove_list_duplicates.js
@@ -1,37 +1,34 @@
 const LinkedList = require("../util/LinkedListX");
 
 function removeDuplicates(list) {
-  const _set = new Set();
-  let cur = list.head;
-  let prev = null;
-  while (cur) {
-    if (_set.has(cur.value)) {
-      // duplicate found
-      // de-link it from the list
-      // cur jumps next but previous stays
-      // right behind cur (as always)
-      let elem = cur;
-      prev.next = cur.next;
-      cur = cur.next;
-      elem.next = null;
-    }
-    else {
-      // add to the set
-      _set.add(cur.value);
-      prev = cur;
-      cur = cur.next;
-    }
-  }
+    const seenValues = new Set();
+    let currentNode = list.head;
+    let previousNode = null;
 
-  return list;
+    while (currentNode) {
+        if (seenValues.has(currentNode.value)) {
+            // Duplicate found; remove it from the list
+            previousNode.next = currentNode.next;
+            currentNode.next = null; // Clean up reference
+        } else {
+            // Add value to the set and move previous pointer
+            seenValues.add(currentNode.value);
+            previousNode = currentNode;
+        }
+        currentNode = currentNode.next; // Move to the next node
+    }
+
+    return list;
 }
 
-// quick test
-let list = new LinkedList();
-for (let elem of [1, 5, 1, 6, 8, 6, 8, 8, 8, 8]) {
-  list.append(elem);
+// Example usage in a real-world application: cleaning up user input
+function cleanUserInput(inputArray) {
+    const uniqueList = new LinkedList();
+    inputArray.forEach(item => uniqueList.append(item));
+    return removeDuplicates(uniqueList)._toArray();
 }
 
-removeDuplicates(list);
-
-console.log(list._toArray()); // [1, 5, 6, 8]
+// Quick test
+const userInput = [1, 5, 1, 6, 8, 6, 8, 8, 8, 8];
+const cleanedInput = cleanUserInput(userInput);
+console.log(cleanedInput); // Output: [1, 5, 6, 8]
