f4956514645c84620c93ed9f728b1548506eca5e
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-04-03 22:58:24 +0300

    Update datastructures.js

--- JavaScript basics/datastructures.js
+++ JavaScript basics/datastructures.js
@@ -113,17 +113,14 @@ const drinkValue = testObj["the drink"];
 function zeroArray(m, n) {
   let newArray = [];
   for (let i = 0; i < m; i++) {
-    let row = []; /* row has been declared inside the outer loop. 
-     Now a new row will be initialised during each iteration of the outer loop allowing for the desired matrix. */
+    let row = []; // A new row is initialized for each iteration
     for (let j = 0; j < n; j++) {
-      row.push(3);
+      row.push(3); // Fill the row with the value 3
     }
-    newArray.push(row);
+    newArray.push(row); // Add the filled row to the newArray
   }
   return newArray;
 }
-let matrix = zeroArray(3, 3);
-console.log(matrix); // output [ [ 3, 3, 3 ], [ 3, 3, 3 ], [ 3, 3, 3 ] ]
 // ---------------------------------------------------------------------------------------------------------------------------
 
 function htmlColorNames(arr) {
