d65acc77290bafd12ff973f787a8582ab43d2e93
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-04-04 02:04:20 +0300

    Update strings_to_url.js

--- JavaScript basics/strings_to_url.js
+++ JavaScript basics/strings_to_url.js
@@ -1,10 +1,10 @@
 // Functional Programming to Convert Strings to URL Slugs
 function urlSlug(title) {
   return title
-    .split(" ")
-    .filter(substr => substr !== "")
-    .join("-")
-    .toLowerCase();
+    .split(" ") // Split the title into an array of words
+    .filter(substr => substr !== "") // Remove any empty strings from the array
+    .join("-") // Join the words with hyphens
+    .toLowerCase(); // Convert the entire string to lowercase
 }
 
 console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone")); // a-mind-needs-books-like-a-sword-needs-a-whetstone
