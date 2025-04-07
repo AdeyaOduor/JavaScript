6cbd0b6f4e14c6d46ea2107b887c6ef5ca745110
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-04-08 00:32:39 +0300

    Update uniqueCharacters.js

--- stings/uniqueCharacters.js
+++ stings/uniqueCharacters.js
@@ -1,15 +1,12 @@
 var allUniqueChars = function(string) {
-  
-  /* O(n^2) approach, no additional data structures used
-     for each character, check remaining characters for duplicates */
   for (var i = 0; i < string.length; i++) {
     for (var j = i + 1; j < string.length; j++) {
       if (string[i] === string[j]) {
-        return false; // if match, return false
+        return false; // Duplicate found
       }
     }
   }
-  return true; // if no match, return true
+  return true; // All characters are unique
 };
 
 const everyCharUnique = (str, indexOffset = 'a'.charCodeAt()) => {
