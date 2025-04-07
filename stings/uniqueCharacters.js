7ebbcbeb305069ce05152e676858c2dc620d7340
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-04-08 00:36:05 +0300

    Update uniqueCharacters.js

--- stings/uniqueCharacters.js
+++ stings/uniqueCharacters.js
@@ -1,3 +1,4 @@
+// Bruteforce approach:
 var allUniqueChars = function(string) {
   for (var i = 0; i < string.length; i++) {
     for (var j = i + 1; j < string.length; j++) {
@@ -8,28 +9,28 @@ var allUniqueChars = function(string) {
   }
   return true; // All characters are unique
 };
-
+// Bit Manipulation Approach:
 const everyCharUnique = (str, indexOffset = 'a'.charCodeAt()) => {
     let counterTable = Number();
     for(let index of [...str].map(c => c.charCodeAt() - indexOffset)) {
         const mask = 1 << index;
         if(counterTable & mask)
-            return false;
-        counterTable |= mask;
+            return false; // Duplicate found
+        counterTable |= mask; // Set the bit for this character
     }
-    return true;
+    return true; // All characters are unique
 };
-
+// Object/Hash Map Approach:
 function everyCharUnique(str) {
   let obj = {};
   for (let i = 0; i < str.length; i++) {
     if (obj[str[i]] && obj[str[i]] >= 1) {
-      return false;
+      return false; // Duplicate found
     } else {
-      obj[str[i]] = 1;
+      obj[str[i]] = 1; // Mark character as seen
     }
   }
-  return true;
+  return true; // All characters are unique
 }
 
 /* TESTS */
