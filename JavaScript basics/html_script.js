c0da1c75883bda01dd01a9d199375a4ed258489c
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-06-01 14:30:38 +0300

    Update html_script.js

--- JavaScript basics/html_script.js
+++ JavaScript basics/html_script.js
@@ -1,5 +1,7 @@
-// put this script inside .html file but you can also link it externally by adding
-// <script src="path/to/my/script.js"></script>
+/*
+put this script inside .html file but you can also link it externally by adding:
+<script src="path/to/my/script.js"></script>
+*/ 
 <script>
   window.addEventListener("load", () => {
     console.log(".....loading" });
