f18c0708b937d068f8f179767322fa6ca0e534e0
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-07-30 13:03:37 +0300

    Update postgre_linux.js

--- JavaScript basics/postgre_linux.js
+++ JavaScript basics/postgre_linux.js
@@ -1,37 +1,3 @@
-// Create person object
-const Person = function(firstAndLast) {
-  this._firstName = firstAndLast.split(' ')[0];
-  this._lastName = firstAndLast.split(' ')[1];
-
-  this.getFirstName = function() {
-    return this._firstName;
-  };
-
-  this.getLastName = function() {
-    return this._lastName;
-  };
-
-  this.getFullName = function() {
-    return `${this._firstName} ${this._lastName}`;
-  };
-
-  this.setFirstName = function(first) {
-    this._firstName = first;
-    return this._firstName;
-  };
-
-  this.setLastName = function(last) {
-    this._lastName = last;
-    return this._lastName;
-  };
-
-  this.setFullName = function(firstAndLast) {
-    this._firstName = firstAndLast.split(' ')[0];
-    this._lastName = firstAndLast.split(' ')[1];
-    return `${this._firstName} ${this._lastName}`;
-  };
-};
-
 /*
 Verifying PostgreSQL Installation and Running Applications on Linux
 1. Verify PostgreSQL Installation
