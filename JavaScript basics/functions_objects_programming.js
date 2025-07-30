a218a271785af71f86590daaed6383e4920a62c1
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-07-30 13:05:21 +0300

    Update and rename functional_programming.js to functions_objects_programming.js

--- JavaScript basics/functions_objects_programming.js
+++ JavaScript basics/functions_objects_programming.js
@@ -1,3 +1,37 @@
+// Create person object
+const Person = function(firstAndLast) {
+  this._firstName = firstAndLast.split(' ')[0];
+  this._lastName = firstAndLast.split(' ')[1];
+
+  this.getFirstName = function() {
+    return this._firstName;
+  };
+
+  this.getLastName = function() {
+    return this._lastName;
+  };
+
+  this.getFullName = function() {
+    return `${this._firstName} ${this._lastName}`;
+  };
+
+  this.setFirstName = function(first) {
+    this._firstName = first;
+    return this._firstName;
+  };
+
+  this.setLastName = function(last) {
+    this._lastName = last;
+    return this._lastName;
+  };
+
+  this.setFullName = function(firstAndLast) {
+    this._firstName = firstAndLast.split(' ')[0];
+    this._lastName = firstAndLast.split(' ')[1];
+    return `${this._firstName} ${this._lastName}`;
+  };
+};
+
 // Define a function and calling it
 function reusableFunction() {
   console.log("Hi World");
