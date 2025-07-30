4f553130f09124d4fac860582900d96502a1179b
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-07-30 15:11:50 +0300

    Update React_BudgetTracker_Sql.js

--- React_BudgetTracker_Sql.js
+++ React_BudgetTracker_Sql.js
@@ -843,7 +843,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './App.js';
 
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
 
 
