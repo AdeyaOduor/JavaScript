4c5106c4e3a8f7f609449c4754d50dcbe5c73859
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2026-02-12 12:07:58 +0300

    Update fibonacci.js

--- maths/fibonacci.js
+++ maths/fibonacci.js
@@ -73,3 +73,14 @@ console.log('76.4%:', fibonacciRetracements[4]);
 50%: 90
 61.8%: 92.36
 76.4%: 95.28*/
+
+// Getting the millionth fibonacci
+const fib = n =>
+  (fn => n < 0 && !(n % 2) ? -fn(Math.abs(n))[0] : fn(Math.abs(n))[0])
+  (function fn(n) {
+    if (!n) return [0n, 1n];
+    const [_n, _n1] = fn(n >> 1);
+    const _2n = _n * (2n * _n1 - _n);
+    const _2n1 = _n ** 2n + _n1 ** 2n;
+    return n % 2 ? [_2n1, _2n + _2n1] : [_2n, _2n1];
+  });
