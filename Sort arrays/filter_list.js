5944b12f79e18e9bca2c36efe49eb00d0f5288df
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2026-02-20 10:58:59 +0300

    Update filter_list.js

--- Sort arrays/filter_list.js
+++ Sort arrays/filter_list.js
@@ -1,3 +1,27 @@
+ /*Implement the function unique_in_order which takes as argument a sequence and returns 
+  a list of items without any elements with the same value next to each other and preserving the original order of elements.
+*/
+
+var uniqueInOrder=function(iterable){
+ 
+   const result = []
+  
+  for(let i = 0; i < iterable.length; i++){
+    if(iterable[i] !== iterable[i + 1]){
+      result.push(iterable[i])
+    }
+  }
+  
+  return result
+}
+// ----------------------------------------------------------------------------
+
+var uniqueInOrder=function(iterable){
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------
 //function taking a list of non-negative integers,strings and returns a new list with the strings filtered out.
 function filter_list(l) {
     // Return a new array with the strings filtered out
