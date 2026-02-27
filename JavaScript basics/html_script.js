20b6f960569bdcbca39a08e1b3693a2b6d7c5d0a
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2026-02-27 11:08:01 +0300

    Delete JavaScript basics/html_script.js

--- JavaScript basics/html_script.js
+++
@@ -1,8 +0,0 @@
-/*
-put this script inside .html file but you can also link it externally by adding:
-<script src="path/to/my/script.js"></script>
-*/ 
-<script>
-  window.addEventListener("load", () => {
-    console.log(".....loading" });
-</script>
